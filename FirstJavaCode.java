5b76813716e4e2476f0ffc3fc6c854d7761253c9
Anoop Lamba <anooplamba77@gmail.com>
2022-08-15 00:21:19 +0530

    initial commit

---
+++ FirstJavaCode.java
@@ -0,0 +1,9 @@
+public class FirstJavaCode{
+    public static int sumOfTwoNumbers(int x, int y) {
+        return x+y;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumOfTwoNumbers(12,10));
+    }
+}
