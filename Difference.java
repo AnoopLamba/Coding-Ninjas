f376e035e5d2c641539fb550f553795a4b91ad97
Anoop Lamba <anooplamba77@gmail.com>
2022-08-24 07:42:17 +0530

    removed 3 temporary files

--- Difference.java
+++
@@ -1,5 +0,0 @@
-public class Difference {
-    public static void main(String[] args) {
-        System.out.println(12-10);
-    }
-}
--- FirstJavaCode.java
+++
@@ -1,9 +0,0 @@
-public class FirstJavaCode{
-    public static void sumOfTwoNumbers(int x, int y) {
-        System.out.println(x+y);
-    }
-
-    public static void main(String[] args) {
-        sumOfTwoNumbers(1,2);
-    }
-}
