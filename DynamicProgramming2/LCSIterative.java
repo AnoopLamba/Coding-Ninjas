5026eea1e54aa2ccfb38c8f705284fff434beb39
Anoop Lamba <anooplamba77@gmail.com>
2022-09-03 21:13:48 +0530

    updated LCS iterative code

--- DynamicProgramming2/LCSIterative.java
+++ DynamicProgramming2/LCSIterative.java
@@ -8,19 +8,24 @@ public class LCSIterative {
 
         for(int i = m-1; i>=0; i--) {
             for(int j = n-1; j>=0; j--) {
-                int ans = 0;
+                int ans;
                 if(s1.charAt(i) == s2.charAt(j)) {
                     ans = 1+arr[i+1][j+1];
                 } else {
                     int out1 = arr[i][j+1];
-                    int out2 = arr[i+1][j+1];
+                    int out2 = arr[i+1][j];
                     ans = Math.max(out1, out2);
                 }
                 arr[i][j] = ans;
             }
         }
 
-        return arr[0][0];
-        
+        return arr[0][0];  
+    }
+
+    public static void main(String[] args) {
+        String s1 = "adebc";
+        String s2 = "dcadb";
+        System.out.println(lcs(s1, s2));
     }
 }
